10f2ab63373cd08433098675eddc823fb483ae68
sahanirakesh877 <91953659+sahanirakesh877@users.noreply.github.com>
2024-02-16 16:48:11 +0545

    content

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className="banner container mx-auto">
       <div className="row d-flex flex-column-reverse flex-lg-row justify-content-between align-items-center">
         <div className="col-lg-6 mb-3 mb-lg-0 border-end  border-2  ">
-          <h1 className="text-capitalize"> The only made in india 4k laparoscopic system</h1>
+          <h1 className="text-capitalize fw-bolder head"> The only make in india 4k laparoscopic system</h1>
           <p className="mb-3">
           It's time to change  your old laproscopic system into new gen 4k system
           </p>
